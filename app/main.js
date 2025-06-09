e6860137041de145d2c36c2fecc73199f58e5751
Shambalam <dan.cararo@gmail.com>
2025-06-09 12:56:47 -0400

    1

--- app/main.js
+++ app/main.js
@@ -5,9 +5,14 @@ console.log("Logs from your program will appear here!");
 
 //Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+      socket.write("HTTP/1.1 200 OK \r\n\r\n");
+      socket.end();
+  });
   socket.on("close", () => {
     socket.end();
   });
 });
 
 server.listen(4221, "localhost");
+
