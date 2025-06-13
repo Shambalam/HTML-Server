4c8c375197c4e7d7ca73d6a11598796ecc17d0ff
Shambalam <dan.cararo@gmail.com>
2025-06-13 00:06:49 -0400

    2

--- app/main.js
+++ app/main.js
@@ -6,6 +6,10 @@ console.log("Logs from your program will appear here!");
 //Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
+      const data = data.toString().split("\r\n")
+      [a, b, c] = data[0].split(" ");
+      console.log({method:a,path:b,version:c});
+      
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
       socket.end();
   });
